8eed73a62c2baacb9963b7489abe53ddef8b64fa
Björn <bjornogreen@gmail.com>
2017-04-04 15:17:26 +0200

    added GenerateMonster to class monster

--- DungeonsOfDoom/Monster.cs
+++ DungeonsOfDoom/Monster.cs
@@ -8,13 +8,19 @@ namespace DungeonsOfDoom
 {
     class Monster : Organism
     {
+        static Random rnd = new Random();
+
         public Monster(string name, char icon, int health, int attack) : base(name, icon, health, attack) 
         {
-            Random rnd = new Random();
             if (rnd.Next(0,101) < 100) 
             {
                 Inventory.Add(Item.GenerateItem());
             }
+        }
+
+        public static Monster GenerateMonster()
+        {
+            return new Monster("Monster", 'M', 10, rnd.Next(1, 10));
         }
     }
 }
