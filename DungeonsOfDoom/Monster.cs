4e956e399f5cf43188899e8d4906d50a441ae600
elinnea <elinnea.larsson@gmail.com>
2017-04-05 13:09:53 +0200

    förändrade metoden Attack

--- DungeonsOfDoom/Monster.cs
+++ DungeonsOfDoom/Monster.cs
@@ -9,7 +9,7 @@ namespace DungeonsOfDoom
     abstract class Monster : Organism
     {
         protected static Random rnd = new Random();
-
+        
         public Monster(string name, char icon, int health, int strength) : base(name, icon, health, strength) 
         {
             if (rnd.Next(0,101) < 10) 
@@ -28,9 +28,8 @@ namespace DungeonsOfDoom
                     return new Toucan();
                 case 2:
                     return new RubberDuck();
-                default:
-                    break;
             }
+            return null;
         }
     }
 }
--- DungeonsOfDoom/Organism.cs
+++ DungeonsOfDoom/Organism.cs
@@ -15,15 +15,12 @@ namespace DungeonsOfDoom
             Inventory = new List<Item>();
         }
 
-        public int Health { get; set; }
+        public int Health { get; set;}
+        
         public int Strength { get; set; }
         public List<Item> Inventory { get; private set; }
         public bool IsAlive { get { return Health > 0; } }
 
-        public virtual string Attack(Organism opponent)
-        {
-            return $"The monster hits you for {Strength} hp";
-        }
-
+        public abstract string Attack(Organism opponent);
     }
 }
